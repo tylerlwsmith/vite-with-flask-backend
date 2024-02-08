b7f678d1197f53493e222169810163bb312957a6
Tyler Smith <tylerlwsmith@gmail.com>
2024-02-07 18:56:35 -0800

    Code formatting w/ black

--- main.py
+++ main.py
@@ -19,38 +19,42 @@ if is_production:
     try:
         with open(manifest_path, "r") as content:
             manifest = json.load(content)
-    except OSError as exc:
-        raise OSError(f"Manifest file not found at {manifest_path}. Run `npm run build`.") from exc
-
-
+    except OSError as exception:
+        raise OSError(
+            f"Manifest file not found at {manifest_path}. Run `npm run build`."
+        ) from exception
 
 # Set up application.
 app = Flask(
     __name__,
-    static_url_path='',
-    static_folder='public',
-    template_folder='templates')
+    static_url_path="",
+    static_folder="public",
+    template_folder="templates",
+)
+
 
 # Add template function for locating Vite URLs.
 @app.context_processor
 def vite_urls():
     def dev(file_path):
         return f"{VITE_SERVER_ORIGIN}/{file_path}"
-    
+
     def prod(file_path):
         try:
-            return f"/assets/{manifest[file_path]["file"]}"
+            return "/assets/{}".format(manifest[file_path]["file"])
         except:
             return "asset-not-found"
-            
+
     return dict(asset=prod if is_production else dev)
+
 
 # Add template variables.
 @app.context_processor
 def env():
     return dict(is_production=is_production)
 
+
 # Setup application routes.
-@app.get('/')
+@app.get("/")
 def homepage():
     return render_template("homepage.html")
