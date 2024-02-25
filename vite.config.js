36e8cb3e6772e8412c37e603ef9f1c91ddd97ca5
Tyler Smith <tylerlwsmith@gmail.com>
2024-02-25 14:35:13 -0800

    Reorder config to match blog post

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: path.join(__dirname, "./assets_source"),
+  root: path.join(__dirname, "./assets_source/"),
   base: "/assets/",
   build: {
+    outDir: path.join(__dirname, "./assets_compiled/"),
     manifest: "manifest.json",
-    outDir: path.join(__dirname, "./assets_compiled"),
     assetsDir: "bundled",
-    emptyOutDir: true,
-    copyPublicDir: true,
     rollupOptions: {
-      input: ["assets_source/scripts/app.ts", "assets_source/styles/app.scss"],
+        input: [
+          "assets_source/script.ts",
+          "assets_source/styles.scss",
+        ],
     },
+    emptyOutDir: true,
+    copyPublicDir: false,
   },
 });
